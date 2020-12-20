41da0e46af83dbc73a0ae718abc0bdf278a38f24
H.Komine <hk0mine@outlook.jp>
2020-12-20 12:55:10 +0900

    Update: 定期実行用にスクリプトを修正

--- db/scripts/script.py
+++ db/scripts/script.py
@@ -1,8 +1,15 @@
 import requests
-from tqdm import tqdm
 import db
+import datetime
 
 OPENBD_ENDPOINT = 'https://api.openbd.jp/v1/'
+
+
+def printLog(message):
+    now = datetime.datetime.today()
+    print('{} {}'.format(
+        now.strftime("%Y/%m/%d %H:%M:%S"),
+        message))
 
 
 def chunked(iterable, n):
@@ -30,21 +37,25 @@ def get_bibs(items):
 
 
 if __name__ == '__main__':
+    printLog('書誌情報の収集開始')
     coverage = get_coverage()
-    print(len(coverage))
+    printLog('covrege: {}'.format(len(coverage)))
     known_isbn_list = db.get_isbn_list()
-    print(len(known_isbn_list))
+    printLog('known_isbn_list: {}'.format(len(known_isbn_list)))
     coverage = list(set(coverage) - set(known_isbn_list))
-    print(len(coverage))
+    printLog('新規に取得する書誌情報の数: {}'.format(len(coverage)))
 
     # ISBNのリストを10000件単位に分割
     chunked_coverage = chunked(coverage, 10000)
 
     cnt = 0
     for isbn_list in chunked_coverage:
-        print('Processing chunk {}/{}'.format(cnt + 1, len(chunked_coverage)))
+        printLog('chunk {}/{}'.format(cnt + 1, len(chunked_coverage)))
+        printLog('get_bibs start')
         results = get_bibs(isbn_list)
-        for book in tqdm(results):
+        printLog('get_bibs end')
+        printLog('Adding books to database')
+        for book in results:
             if not 'summary' in book:
                 continue
             if book['summary']['isbn'] in known_isbn_list:
@@ -53,3 +64,4 @@ if __name__ == '__main__':
             db.insert_book(book)
 
         cnt = cnt + 1
+    printLog('書誌情報の収集完了')
