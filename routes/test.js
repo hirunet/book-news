65045cb973c5db51d2461bf3d23d5bec421fa324
H.Komine <hk0mine@outlook.jp>
2020-12-28 23:36:08 +0900

    Add Bluma test page

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('test');
+  // res.send('test');
+  res.render('test');
 });
 
 module.exports = router;
