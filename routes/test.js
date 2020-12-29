e53130f50eeaddaf509869c47124a4df5e264b4e
H.Komine <hk0mine@outlook.jp>
2020-12-29 13:55:49 +0900

    Update: Blumaお試しページに検索フォームを追加

--- routes/test.js
+++ routes/test.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const booksModel = require("../models/books");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  // res.send('test');
-  res.render('test');
-});
+async function searchBooks(req, res, next) {
+  const books = await booksModel.getBooks(req.query);
+  
+  const books2 = books.map(function(book) {
+    const data = book.data_json;
+    if (! data.onix) 
+      return book;
+    if (! data.onix.CollateralDetail) 
+      return book;
+    if (! data.onix.CollateralDetail.TextContent) 
+      return book;
 
+    for (var textContent of data.onix.CollateralDetail.TextContent) {
+      if (textContent.TextType == "03" ) {
+        // textContent = textContent.Text.replace( /\r?\n/g, '<br />' );
+        const MAX_LENGTH = 140;
+        let text = textContent.Text;
+        if (text.length > MAX_LENGTH) {
+          text = text.substr(0, MAX_LENGTH) + "……";
+        }
+        book.text = text;
+        break;
+      }
+    }
+    return book;
+  });
+
+  res.render("test", { books: books2 });
+}
+
+router.get('/', searchBooks);
 module.exports = router;
