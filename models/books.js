0f2f373377e023c4556167706034fac1fab130d5
H.Komine <hk0mine@outlook.jp>
2020-12-29 11:18:53 +0900

    Update: コメント追記、検索範囲の開始時刻を修正

--- models/books.js
+++ models/books.js
@@ -7,8 +7,11 @@ const database = process.env.DATABASE_URL;
  * DBから検索条件にあった書籍のリストを取得する
  * @param {Object} query 検索条件
  *    以下のプロパティを指定できる
- *    - タイトル
- *    - Cコード
+ *    - {String} title タイトル
+ *    - {String} ccode Cコード
+ *    TODO 以下を実装する
+ *    - {String} fromDate 検索範囲の開始日時（デフォルトは二週間前）
+ *    - {String} toDate   検索範囲の終了日時（デフォルトは当日）
  * @return {Array.<Object>} books 取得した書籍のリスト
  */
 async function getBooks(query) {
@@ -21,9 +24,9 @@ async function getBooks(query) {
 
   // Filter by pubdate
   statement += ' WHERE $1 < pubdate AND pubdate <= $2';
-  let fromDate = moment().add(-10, 'day').format("YYYYMMDD");
+  let fromDate = moment().add(-14, 'day').format("YYYYMMDD");
   if (query.title) {
-    fromDate = moment().add(-1000000000, 'day').format("YYYYMMDD");
+    fromDate = moment().add(-1000, 'day').format("YYYYMMDD");
   }
   const today = moment().format("YYYYMMDD");
   params.push(fromDate);
