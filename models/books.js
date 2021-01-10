c64a891b85fd56f10b91083d3bcd8ba7f183a47a
H.Komine <hk0mine@outlook.jp>
2021-01-10 18:40:29 +0900

    Update: APIサーバ用に不要なファイルの削除、コードの整理

--- models/books.js
+++ models/books.js
@@ -1,7 +1,7 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 const moment = require("moment");
 
-const database = process.env.DATABASE_URL;
+const database = process.env.DATABASE_URL2;
 
 /**
  * DBから検索条件にあった書籍のリストを取得する
@@ -16,17 +16,17 @@ const database = process.env.DATABASE_URL;
  */
 async function getBooks(query) {
   const client = new Client({
-    connectionString: database
+    connectionString: database,
   });
 
-  let statement = 'SELECT * FROM books';
+  let statement = "SELECT * FROM books";
   let params = [];
 
   // Filter by pubdate
-  statement += ' WHERE $1 < pubdate AND pubdate <= $2';
-  let fromDate = moment().add(-14, 'day').format("YYYYMMDD");
+  statement += " WHERE $1 < pubdate AND pubdate <= $2";
+  let fromDate = moment().add(-14, "day").format("YYYYMMDD");
   if (query.title) {
-    fromDate = moment().add(-1000, 'day').format("YYYYMMDD");
+    fromDate = moment().add(-1000, "day").format("YYYYMMDD");
   }
   const today = moment().format("YYYYMMDD");
   params.push(fromDate);
@@ -34,16 +34,16 @@ async function getBooks(query) {
 
   // Filter by title
   if (query.title) {
-    statement += ' AND title LIKE $3';
-    params.push('%' + query.title + '%');
+    statement += " AND title LIKE $3";
+    params.push("%" + query.title + "%");
   }
   // Filter by ccode
   else if (query.ccode) {
-    statement += ' AND ccode LIKE $3';
-    params.push('%' + query.ccode + '%');
+    statement += " AND ccode LIKE $3";
+    params.push("%" + query.ccode + "%");
   }
 
-  statement += ' ORDER BY pubdate DESC, isbn;';
+  statement += " ORDER BY pubdate DESC, isbn;";
 
   await client.connect();
   const result = await client.query(statement, params);
@@ -59,37 +59,17 @@ async function getBooks(query) {
  */
 async function getBookByIsbn(isbn) {
   const client = new Client({
-    connectionString: database
+    connectionString: database,
   });
 
-  let statement = 'SELECT * FROM books WHERE isbn = $1;';
+  let statement = "SELECT * FROM books WHERE isbn = $1;";
 
   await client.connect();
-  const result = await client.query(statement, [isbn, ]);
+  const result = await client.query(statement, [isbn]);
   await client.end();
 
   return result.rows[0];
 }
 
-/**
- * DBから指定した著者名の書籍のリストを取得する
- * @param {String} author 著者名
- * @return {Array.<Object>} books 取得した書籍のリスト
- */
-async function getBooksByAuthor(author) {
-  const client = new Client({
-    connectionString: database
-  });
-
-  let statement = 'SELECT * FROM books WHERE author = $1 ORDER BY pubdate DESC;';
-
-  await client.connect();
-  const result = await client.query(statement, [author, ]);
-  await client.end();
-  console.log(result);
-  return result.rows;
-}
-
 exports.getBooks = getBooks;
 exports.getBookByIsbn = getBookByIsbn;
-exports.getBooksByAuthor = getBooksByAuthor;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const booksModel = require("../models/books");
 
-async function searchBooks(req, res, next) {
-  const books = await booksModel.getBooks(req.query);
-  if (req.query.style) {
-    style = req.query.style;
-  } else {
-    style = "list";
-  }
-  res.render("index", { books: books, style: style });
+function sayHello(req, res, next) {
+  res.send("Hello, world!");
 }
 
-router.get('/', searchBooks);
+async function getBooks(req, res, next) {
+  const books = await booksModel.getBooks(req.query);
+  if (!books) {
+    res.status(404).send("Not found");
+    return;
+  }
+  res.json(books);
+}
+
+async function getBook(req, res, next) {
+  const isbn = +req.params.isbn;
+  const book = await booksModel.getBookByIsbn(isbn);
+  if (!book) {
+    res.status(404).send("Not found");
+    return;
+  }
+  res.json(book);
+}
+
+router.get("/", sayHello);
+router.get("/books", getBooks);
+router.get("/books/:isbn", getBook);
 
 module.exports = router;
